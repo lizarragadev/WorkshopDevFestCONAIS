94dcacb9cca3b5279101a7e376a5b7a81883197d
Gustavo <lizarraga.gux@gmail.com>
2019-10-03 13:51:12 -0400

    Base code Demo Custom 1

--- Custom1/app/src/main/java/com/miramicodigo/custom1/MainActivity.kt
+++ Custom1/app/src/main/java/com/miramicodigo/custom1/MainActivity.kt
@@ -9,9 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        supportActionBar!!.hide()
+
     }
 }
