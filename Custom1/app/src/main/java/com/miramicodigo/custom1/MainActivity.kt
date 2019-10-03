81b86a66b300a35d1d09e445f22494b2598aeedb
Gustavo <lizarraga.gux@gmail.com>
2019-10-03 13:43:37 -0400

    Add demo Custom 1

---
+++ Custom1/app/src/main/java/com/miramicodigo/custom1/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.miramicodigo.custom1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        supportActionBar!!.hide()
+    }
+}
